82a6f0929bda7999597a0fe502e88199b93725fa
loic <96543753+lohhiiccc@users.noreply.github.com>
2025-02-05 02:43:54 +0100

    fix: auto rs detection

--- feature/autoRs/autors.js
+++ feature/autoRs/autors.js
@@ -8,7 +8,6 @@ import "./beuldAutoRscommand"
 // NEED: Party > (optional rank) (name): (optional whitespace)(some command prefix)(dt)(optional reason)
 const dtRegex = /^Party > (\[[\w+]+\] )*[\w_]+: \s*[!.?]*dt.*/
 register("chat", () => {
-    console.log(`dt regex match party leader: ${Party.leader}`);
     if (Party.leader !== Player.getName()) return;
     if (autoRsSettings.isAutoRsRunningD || autoRsSettings.isAutoRsRunningK) {
         autoRsSettings.isAutoRsRunningK = false;
@@ -17,9 +16,7 @@ register("chat", () => {
     }
 }).setCriteria(dtRegex)
 
-const kuudraDown = "&r&f                               &r&6&lKUUDRA DOWN!&r";
 register("chat", (message) => {
-    if (message !== kuudraDown) return;
     if (!autoRsSettings.isAutoRsRunningK) return;
     if (autoRsSettings.autoRsAnnounceK) ChatLib.command(`pc restart in ${autoRsSettings.autoRsDowntimeK}s!`);
     setTimeout(() => {
@@ -29,7 +26,7 @@ register("chat", (message) => {
     }, autoRsSettings.autoRsDowntimeK * 1000)
 
     console.log(message);
-})
+}).setCriteria(/^\s+KUUDRA DOWN!/)
 
 /*---------*/
 /* dungeon */
