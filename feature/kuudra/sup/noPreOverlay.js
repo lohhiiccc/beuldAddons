4d75c7790306ef8a82cd14efbf41193ead54cf97
loic <96543753+lohhiiccc@users.noreply.github.com>
2025-02-05 04:04:56 +0100

    feat: no pre overlay => improve edit postion gui

--- feature/kuudra/sup/noPreOverlay.js
+++ feature/kuudra/sup/noPreOverlay.js
@@ -22,12 +22,52 @@ register("dragged", (mx, my, x, y) => {
     data.save()
 })
 
+register("step", () => {
+    if (Settings.missingSuppliesMove.isOpen())
+        missing = "No Pre"
+    else if (missing === "No Pre") {
+        missing = ""
+    }
+}).setFps(2)
+
+let index = 0;
+register("scrolled", (x, y, dir) => {
+    if (!Settings.missingSuppliesMove.isOpen()) return;
+
+    const opt = ["center", "left", "right"];
+    index += dir;
+    if (index < 0) index = 2;
+    if (index > 2) index = 0;
+    data.noPreOverlayAlign = opt[index];
+    data.save();
+    ChatLib.chat(`&r&l&aAlign:&2 ${data.noPreOverlayAlign}`)
+})
+
 registerWhen(register("renderOverlay", () => {
     const [ width, height ] = [
         Renderer.screen.getWidth(),
         Renderer.screen.getHeight()
     ]
 
+    const textWidth = Renderer.getStringWidth(missing);
     Renderer.scale(2);
-    Renderer.drawString(missing, data.noPreOverlayX, data.noPreOverlayY, true);
-}), () => Settings.missingSuppliesToggle && getKPhase() === Phase.SUPPLIES)
+    switch (data.noPreOverlayAlign) {
+        case "center":
+            const centeredX = data.noPreOverlayX - (textWidth * 0.5);
+            Renderer.drawString(missing, centeredX, data.noPreOverlayY, true);
+            break;
+        case "left":
+            Renderer.drawString(missing, data.noPreOverlayX, data.noPreOverlayY, true);
+            break;
+        case "right":
+            const rightX = data.noPreOverlayX - textWidth;
+            Renderer.drawString(missing, rightX, data.noPreOverlayY, true);
+            break;
+    }
+
+    if (!Settings.missingSuppliesMove.isOpen()) return // config gui code below
+    Renderer.scale(2);
+    Renderer.drawLine(Renderer.RED, 0, data.noPreOverlayY, width, data.noPreOverlayY, 0.5);
+    Renderer.scale(2);
+    Renderer.drawLine(Renderer.RED, data.noPreOverlayX, 0, data.noPreOverlayX, height, 0.5);
+}), () => Settings.missingSuppliesToggle && (getKPhase() === Phase.SUPPLIES || Settings.missingSuppliesMove.isOpen()))
--- utils/data.js
+++ utils/data.js
@@ -4,6 +4,7 @@ export let data = new PogObject("beuld", {
     KBgfsPearl: 0,
     noPreOverlayX: 100,
     noPreOverlayY: 100,
+    noPreOverlayAlign: "center",
 })
 
 register("gameUnload", () => {
